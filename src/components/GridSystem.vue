dd17c0f0d0b37fb91990909dcf45e3da5baa95da
mohandshahin18 <sha7in147@gmail.com>
2023-09-17 21:13:15 +0300

    Grid System

---
+++ src/components/GridSystem.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        xl="3"
+        xxl="3"
+        style="border: 1px solid"
+      >
+        Test one
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        xl="3"
+        xxl="3"
+        style="border: 1px solid"
+      >
+        Test two
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        xl="3"
+        xxl="3"
+        style="border: 1px solid"
+      >
+        Test three
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>This is homepage</h1>
+    <GridSystem />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import GridSystem from "@/components/GridSystem.vue";
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    GridSystem,
   },
 };
 </script>
