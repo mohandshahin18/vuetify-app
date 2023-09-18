80a1b57ae784a8c0c01dfab771f4aa968a1fb7ff
mohandshahin18 <sha7in147@gmail.com>
2023-09-18 16:26:08 +0300

    margin-padding-width class

---
+++ src/components/MarginPadding.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="border: 1px solid; width: 400px" class="ml-16">Test</div>
+  <!-- <div style="border: 1px solid; width: 400px" class="mx-auto">Test</div> -->
+  <!-- <div style="border: 1px solid" class="w-75">Test</div> -->
+  <!-- width : 100 - 75 - 50 - 25 -->
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <h1>This is homepage</h1>
-    <GridSystem />
+    <!-- <GridSystem /> -->
+    <MarginPadding />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import GridSystem from "@/components/GridSystem.vue";
+// import GridSystem from "@/components/GridSystem.vue";
+import MarginPadding from "@/components/MarginPadding.vue";
 export default {
   name: "HomeView",
   components: {
-    GridSystem,
+    // GridSystem,
+    MarginPadding,
   },
 };
 </script>
