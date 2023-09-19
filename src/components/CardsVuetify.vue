2e5683f99b1f21d3490a60825357e21cbd28ed88
mohandshahin18 <sha7in147@gmail.com>
2023-09-20 00:42:36 +0300

    Chip and Divider

--- src/components/CardsVuetify.vue
+++ src/components/CardsVuetify.vue
@@ -11,6 +11,7 @@
             minus adipisci nesciunt tempore ullam nemo, sunt eos et at
             sed.</v-card-text
           >
+          <v-divider length="70%"></v-divider>
           <v-card-actions class="justify-end"
             ><v-btn color="blue" variant="flat">
               <v-icon>mdi-check</v-icon> Save</v-btn
@@ -18,6 +19,12 @@
             <v-btn color="red" variant="flat">
               <v-icon>mdi-delete</v-icon> Delete</v-btn
             ></v-card-actions
+          >
+          <v-divider color="red" inset></v-divider>
+
+          <v-chip color="red" class="my-5" label>
+            <v-icon>mdi-account-outline</v-icon>
+            My first chip</v-chip
           >
         </v-card>
       </v-col>
