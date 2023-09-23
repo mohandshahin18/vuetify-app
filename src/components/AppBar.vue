0fc3ad2a3dffdbe1d1848b871ef7ef79c97de77b
mohandshahin18 <sha7in147@gmail.com>
2023-09-23 23:47:30 +0300

    bottom navigation

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,15 +1,31 @@
 <template>
   <v-layout>
-    <v-app-bar
-      elevation="24"
-      color="teal-darken-4"
-      image="https://picsum.photos/1920/1080?random"
-    >
+    <v-app-bar elevation="24" color="teal-darken-4">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-icon class="mr-4">mdi-magnify</v-icon>
       <v-icon class="mr-4">mdi-heart</v-icon>
       <v-icon>mdi-dots-vertical</v-icon>
     </v-app-bar>
+
+    <v-bottom-navigation bg-color="teal-darken-4">
+      <v-btn>
+        <v-icon>mdi-history</v-icon>
+
+        <span>Recent</span>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-heart</v-icon>
+
+        <span>Favorites</span>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>Nearby</span>
+      </v-btn>
+    </v-bottom-navigation>
   </v-layout>
 </template>
