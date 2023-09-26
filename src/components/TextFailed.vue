a65be6073909b9dfc77cb7dc94d61c5a8aad3b6e
mohandshahin18 <sha7in147@gmail.com>
2023-09-26 12:41:24 +0300

    CheckBox and Radio Button

--- src/components/TextFailed.vue
+++ src/components/TextFailed.vue
@@ -27,6 +27,14 @@
             class="text-start"
           ></v-textarea>
         </v-col>
+        <v-col cols="12">
+          <v-checkbox label="Mohanad"></v-checkbox>
+          <v-radio-group>
+            <v-radio label="Radio 1" value="1"></v-radio>
+            <v-radio label="Radio 2" value="2"></v-radio>
+            <v-radio label="Radio 3" value="3"></v-radio>
+          </v-radio-group>
+        </v-col>
       </v-row>
       <v-btn @click="validate">Submit</v-btn>
     </v-card>
@@ -37,7 +45,6 @@
 import { ref } from "vue";
 const myText = ref("");
 const errorMsg = ref([]);
-
 const validate = () => {
   errorMsg.value = "";
   if (!myText.value) {
