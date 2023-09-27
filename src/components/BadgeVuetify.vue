74576bd1c02dcb020d389210786f73ed8b53f5fa
mohandshahin18 <sha7in147@gmail.com>
2023-09-28 00:59:37 +0300

    Hover

--- src/components/BadgeVuetify.vue
+++ src/components/BadgeVuetify.vue
@@ -1,11 +1,13 @@
 <template>
   <v-container>
     <v-card width="50%" class="mx-auto pa-10" height="200">
-      <v-btn variant="text">
-        <v-badge color="error" content="20" offset-x="2" bordered max="9">
-          <v-icon size="30">mdi-bell</v-icon>
-        </v-badge>
-      </v-btn>
+      <v-hover v-slot="{ isHovering, props }">
+        <v-btn variant="text" v-bind="props" :elevation="isHovering ? 10 : 0">
+          <v-badge color="error" content="20" offset-x="2" bordered max="9">
+            <v-icon size="30">mdi-bell</v-icon>
+          </v-badge>
+        </v-btn>
+      </v-hover>
     </v-card>
   </v-container>
 </template>
