ce08404318533370da19166a00505db21f5dff9e
mohandshahin18 <sha7in147@gmail.com>
2023-10-01 23:28:25 +0300

    Last Edit

--- src/components/BadgeVuetify.vue
+++ src/components/BadgeVuetify.vue
@@ -8,6 +8,9 @@
           </v-badge>
         </v-btn>
       </v-hover>
+      <v-badge color="error" content="20" offset-x="2" bordered max="9">
+        <v-icon size="30">mdi-bell</v-icon>
+      </v-badge>
     </v-card>
   </v-container>
 </template>
